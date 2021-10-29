f8d4a08281bbda430f282c192b777cd6423c5b0f
ronsun <ron.sun@hotmail.com.tw>
2021-10-29 23:55:57 +0800

    Initialize and Empty project for MultipleImplementation

---
+++ MultipleImplementation/Controllers/DemoController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MultipleImplementation.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DemoController : ControllerBase
+    {
+        public DemoController()
+        {
+        }
+
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return Ok("Hello");
+        }
+    }
+}
