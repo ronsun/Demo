d7f4e2c02935fae9af45048296a7f6989ee66622
ronsun <ron.sun@hotmail.com.tw>
2021-11-28 01:12:46 +0800

    Merge sample project (MyCore) for article https://ronsun.github.io/content/20200120-dotnet-command-by-jenkins.html

---
+++ MyCore/MyCore/Controllers/ValuesController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace MyCore.Controllers
+{
+    public class MyLogger<T>
+    {
+        private ILogger<T> _logger;
+
+        public MyLogger(ILogger<T> logger)
+        {
+            _logger = logger;
+        }
+
+        public void WrieteLog(string msg)
+        {
+            _logger.LogCritical(msg);
+        }
+    }
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private ILogger<ValuesController> _logger;
+        private MyLogger<ValuesController> _myLogger;
+
+        public ValuesController(
+            ILogger<ValuesController> logger,
+            MyLogger<ValuesController> myLogger)
+        {
+            _logger = logger;
+            _myLogger = myLogger;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            _logger.LogCritical("Hello world");
+            _myLogger.WrieteLog("Wrapped Hello World");
+
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
